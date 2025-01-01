134bb3a48654a0ac84013a223ff25e64c24dacec
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-01-01 15:58:03 -0500

    repository name change test

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -10,7 +10,7 @@ const GameGrid = () => {
 		<>
 			{error && <p>{error}</p>}
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-				{isLoading && skeletons.map((_, index) => <PokemonCardSkeleton key={index} />)}
+				{isLoading && skeletons.map((_, i) => <PokemonCardSkeleton key={i} />)}
 				{pokemons.map((pokemon) => (
 					<PokemonCard key={pokemon.url} pokemon={pokemon} />
 				))}
