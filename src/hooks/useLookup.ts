05c66594a8f15a2a96b988dfc7f419c94b23cd04
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-01-12 20:05:42 -0500

    added type safety to useLookup hook

--- src/hooks/useLookup.ts
+++ src/hooks/useLookup.ts
@@ -2,7 +2,11 @@ const useLookup = <T extends { [key: string]: unknown }>(
 	items: T[],
 	propertyName: string,
 	value: unknown
-) => {
+): T | undefined => {
+	if (!items || !propertyName || value === undefined) {
+		console.warn('Invalid arguments provided to useLookup.');
+		return undefined;
+	}
 	return items.find((item) => item[propertyName] === value);
 };
 
