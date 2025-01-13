3914e25e55946479747695f9d0d00ff57faa51b1
Hyungju Kwon <hyungju.kwon@gmail.com>
2025-01-12 19:54:13 -0500

    Moere skeletion added

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -29,7 +29,7 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
 	// 	}
 	// }, [inView, hasNextPage, fetchNextPage]);
 
-	const skeletons = [1, 2, 3, 4, 5, 6];
+	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 	const fetchedGameCount =
 		data?.pages.reduce(
